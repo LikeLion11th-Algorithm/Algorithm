d15a8758923b103907478cd4894ab0a10322638a
Hajin-Lee0406 <glgkwls1@naver.com>
2023-03-27 18:56:10 +0900

    [백준]:10926 ??!

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
+        String input = sc.next();
 
-        sc.close();
-
-        System.out.println(A+B);
-        System.out.println(A-B);
-        System.out.println(A*B);
-        System.out.println(A/B);
-        System.out.println(A%B);
+        System.out.println(input+"??!");
     }
 }
